e81b5fcc31d04b1296d76039f9f46358082c76da
OrysyaStus <orysyastus2012@gmail.com>
2016-04-26 01:34:46 -0700

    inequalities code

---
+++ Programming_for_Everybody/Inequalities/num5.py
@@ -0,0 +1,10 @@
+# Inequality practice with the number 5.
+
+num = raw_input('Write number here: ')
+
+if int(num) > 5:
+    print 'Number is greater than 5'
+else:
+    print 'Number is not greater than 5'
+
+print 'Done'
